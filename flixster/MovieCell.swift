1b5d1883f3a74cfc753f99a77afb49687d002ede
typhlosiongo <professionaloscar@gmail.com>
2019-10-18 13:34:12 -0700

    big brain project

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Oscar Ramirez on 10/18/19.
+//  Copyright © 2019 oscar.ramirez.lucero. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
