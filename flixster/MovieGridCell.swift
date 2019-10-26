4961f49e5a59485660448a6133d1d16386f79d4c
typhlosiongo <professionaloscar@gmail.com>
2019-10-25 23:16:35 -0700

    added a new pushnavigation menu as well as a tabbed menu

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Oscar Ramirez on 10/25/19.
+//  Copyright © 2019 oscar.ramirez.lucero. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
